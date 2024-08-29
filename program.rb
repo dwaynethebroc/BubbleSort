c4b6ac001181da7477e1445b4950bb23295da244
dwaynethebroc <127725696+dwaynethebroc@users.noreply.github.com>
2024-08-29 15:58:14 -0700

    not swapping last pair to finish sort

--- program.rb
+++ program.rb
@@ -6,14 +6,12 @@ def bubble_sort(array)
   unless is_sorted == true
     array.each_with_index do |a, index_a|
       array.each_with_index do |b, index_b|
-        if a > b 
+        if a < b 
           array[index_a], array[index_b] = array[index_b], array[index_a]
         end
       end
     end
-
-    count++
-
+    
     if array == array.sort
       is_sorted = true
     end
@@ -23,12 +21,13 @@ def bubble_sort(array)
   return array
 end
 
-test_array_1 = 20.times.map { rand(1..20) }
-test_array_2 = 20.times.map { rand(1..20) }
-test_array_3 = 20.times.map { rand(1..20) }
+test_array_1 = 10.times.map { rand(1..20) }
+test_array_2 = 10.times.map { rand(1..20) }
+test_array_3 = 10.times.map { rand(1..20) }
 
 bubble_sort([4,3,78,2,0,2])
 # => [0,2,2,3,4,78]
-bubble_sort(test_array_1)
-bubble_sort(test_array_2)
-bubble_sort(test_array_3)
+
+#bubble_sort(test_array_1)
+#bubble_sort(test_array_2)
+#bubble_sort(test_array_3)
