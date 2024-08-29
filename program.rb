60c394d089fea0dde803a7e08d6f347ef7664109
dwaynethebroc <127725696+dwaynethebroc@users.noreply.github.com>
2024-08-29 16:38:58 -0700

    removed double loop, do single comparison loop

--- program.rb
+++ program.rb
@@ -1,23 +1,23 @@
 def bubble_sort(array)
 
+  array_size = array.length
   is_sorted = false
   count = 0
 
-  unless is_sorted == true
-    array.each_with_index do |a, index_a|
-      array.each_with_index do |b, index_b|
-        if a < b 
-          array[index_a], array[index_b] = array[index_b], array[index_a]
-        end
+  while !is_sorted
+    (array_size-1).times do |i|
+      if array[i] > array[i+1]
+        array[i], array[i+1] = array[i+1], array[i]
+        count += 1
       end
     end
-    
+
     if array == array.sort
       is_sorted = true
     end
   end
-
-  puts "Number of sort: #{count}"
+  
+  puts "Number of sorts: #{count}"
   return array
 end
 
@@ -28,6 +28,6 @@ test_array_3 = 10.times.map { rand(1..20) }
 bubble_sort([4,3,78,2,0,2])
 # => [0,2,2,3,4,78]
 
-#bubble_sort(test_array_1)
-#bubble_sort(test_array_2)
-#bubble_sort(test_array_3)
+bubble_sort(test_array_1)
+bubble_sort(test_array_2)
+bubble_sort(test_array_3)
